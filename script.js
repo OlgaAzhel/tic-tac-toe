cadae517055e6cdd70e1ea468e28eb4a688456cd
Olga Azhel <olga.azhel@gmail.com>
2023-01-28 18:05:02 -0500

    game works, still needs more testing and code cleanup

---
+++ script.js
@@ -0,0 +1,119 @@
+// 1) Define required constants
+const messageEl = document.querySelector('h1')
+const playAgainBtn = document.querySelector('button')
+const cellEls = [...document.querySelectorAll('#board > div')]
+
+// 2) Define required variables used to track the state of the game
+let board
+let turn
+let winner
+
+let cellState = {
+    '0': null,
+    '-1': 'x',
+    '1': 'o'
+}
+
+winSituations = {
+    '0': [[1, 2], [4, 8], [3, 6]],
+    '1': [[0, 2], [4, 7]],
+    '2': [[0, 1], [5, 8], [4, 6]],
+    '3': [[0, 6], [4, 5]],
+    '4': [[1, 7], [2, 6], [0, 8], [3, 5]],
+    '5': [[3, 4], [2, 8]],
+    '6': [[0, 3], [2, 4], [7, 8]],
+    '7': [[1, 4], [6, 8]],
+    '8': [[0, 4], [2, 5], [6, 7]]
+}
+
+
+// 3) Create event listeners
+document.getElementById('board').addEventListener('click', handleMove)
+playAgainBtn.addEventListener('click', init)
+
+
+// 4) Initialize game
+init()
+
+function init() {
+    board = [0, 0, 0,
+        0, 0, 0,
+        0, 0, 0]
+    turn = 1;
+    winner = null;
+    render()
+}
+
+function render() {
+    renderBoard()
+    renderMessage()
+    renderControls()
+}
+
+function renderBoard() {
+    board.forEach(function (cell, cellIdx) {
+        const cellEl = document.getElementById(cellIdx)
+        cellEl.innerHTML = cellState[board[cellIdx]]
+    })
+
+}
+
+function renderMessage() {
+    if (winner === 'T') {
+        messageEl.innerText = "It's a Tie! Play again."
+    } else if (winner) {
+        messageEl.innerText = `Player ${cellState[winner].toUpperCase()} Wins!`
+    } else {
+        messageEl.innerText = `Player ${cellState[turn].toUpperCase()}'s Turn`
+    }
+}
+
+function renderControls() {
+    playAgainBtn.style.visibility = winner ? 'visible' : 'hidden'
+}
+
+function handleMove(evt) {
+    const cellIdx = cellEls.indexOf(evt.target)
+    if (cellIdx === -1) {
+        return
+    }
+    evt.target.innerText = cellState[turn]
+    board[cellIdx] = turn;
+    turn *= -1;
+    console.log(board)
+    winner = getWinner(cellIdx)
+    render()
+
+}
+
+function getWinner(moveIdx) {
+
+    let win = 0
+    let score
+    let winArray = winSituations[moveIdx]
+    console.log("this is move index:", moveIdx)
+    winArray.forEach(function (arr) {
+        score = 1
+        arr.forEach(function(el) {
+            if (board[moveIdx] === board[el]) {
+                score = score + 1
+                console.log("THIS IS CURRENT SCORE:", score)
+
+            }
+            
+        })
+        if (score === 3) {
+            win = 1
+        }
+
+    })
+
+    if (score !== 3 && board.indexOf(0) === -1) {
+        return 'T'
+    }
+
+    if (win === 1) {
+        return board[moveIdx]
+    }
+
+}
